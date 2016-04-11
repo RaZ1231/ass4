34627b6febe2ad76b4358ea6e997f38eb4bb267b
Raz <raz.s121@gmail.com>
2016-04-12 02:25:24 +0300

    Added resources, tests directories

---
+++ tests/binary/PlusTest.java
@@ -0,0 +1,23 @@
+package binary;
+
+/**
+ * @author Raziel Solomon
+ * @since 12-Apr-16.
+ */
+public class PlusTest {
+    @org.junit.Test
+    public void evaluate() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void assign() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void toStringTest() throws Exception {
+
+    }
+
+}
